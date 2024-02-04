5455e45f39293a38ebaf6d900c6b0634690e3803
Julien Cros <julien.cros86@gmail.com>
2024-02-04 16:10:28 +0100

    feat: navbar working clean

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,13 +17,11 @@ const NavBar = () => {
 		absolute top-0 left-0 duration-300 bg-white shadow-md`}
 		onMouseOver={() => setIsOpen( true)}
 		onMouseOut={() => setIsOpen( false)}
-		>
-
-			{/* TODO: animation border + animation np to nepftune */}
+	>
 				<Link href={"/"} className='flex justify-start pt-10 pl-6'>
 					<p className='text-3xl text-brown border-b-[1px] border-brown'>N</p>
-					<p className={`${isOpen ? "hidden duration-150" : "block duration-150"} text-3xl text-brown border-b-[1px] border-brown`}>p.</p>
-					<p className={`${isOpen ? "block duration-150" : "hidden duration-150"} text-3xl text-brown border-b-[1px] border-brown`}>epftune</p> 
+					<p className={`${isOpen ? "opacity-0 duration-150" : "duration-150"} left-[41px] absolute text-3xl text-brown border-b-[1px] border-brown`}>f.</p>
+					<p className={`${isOpen ? "duration-150" : "opacity-0 duration-150"} absolute left-[41px] text-3xl text-brown border-b-[1px] border-brown`}>epftune</p> 
 				</Link>
 				<div className='flex flex-col justify-center pl-8 items-start text-brown text-xl pt-20 gap-10 pb-5'>
 				<Link href={"/galerie"} className='flex flex-row'>
