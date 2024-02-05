b28c09ae1da100d177de9fde01c3a128b8601deb
Julien Cros <julien.cros86@gmail.com>
2024-02-05 17:05:06 +0100

    fix: on click problem

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,17 +11,25 @@ const NavBar = () => {
   const pathname = usePathname();
 
   return (
-    <div className={`${isOpen ? "bg-black bg-opacity-30 duration-150 fixed top-0 left-0 w-full h-full z-40" : ""}`}
-		onClick={() => setIsOpen(false)}
-		>
+    <div
+      className={`${
+        isOpen
+          ? "bg-black bg-opacity-30 duration-150 fixed top-0 left-0 w-full h-full z-40"
+          : ""
+      }`}
+    >
       <div
         className={`
 		${isOpen ? "w-32 md:w-52" : "w-12 md:w-20"}
-		absolute top-0 left-0 duration-300 bg-white shadow-md z-40`}
+		fixed top-0 left-0 duration-300 bg-white shadow-md z-50`}
         onMouseOver={() => setIsOpen(true)}
         onMouseOut={() => setIsOpen(false)}
       >
-        <Link href={"/"} className="flex justify-start pt-10 pl-3 md:pl-6">
+        <Link
+          href={"/"}
+          className="flex justify-start pt-10 pl-3 md:pl-6"
+          onClick={() => setIsOpen(false)}
+        >
           <p className="text-2xl md:text-3xl text-brown border-b-[1px] border-brown">
             N
           </p>
@@ -41,7 +49,11 @@ const NavBar = () => {
           </p>
         </Link>
         <div className="flex flex-col justify-center pl-4 md:pl-8 items-start text-brown text-xl pt-20 gap-10 pb-5">
-          <Link href={"/galerie"} className="flex flex-row">
+          <Link
+            href={"/galerie"}
+            className="flex flex-row"
+            onClick={() => setIsOpen(false)}
+          >
             <p
               className={`${
                 pathname === "/galerie"
@@ -60,7 +72,11 @@ const NavBar = () => {
               alerie
             </p>
           </Link>
-          <Link href={"/about"} className="flex flex-row">
+          <Link
+            href={"/about"}
+            className="flex flex-row"
+            onClick={() => setIsOpen(false)}
+          >
             <p
               className={`${
                 pathname === "/about"
@@ -80,7 +96,11 @@ const NavBar = () => {
               bout
             </p>
           </Link>
-          <Link href={"/contact"} className="flex flex-row">
+          <Link
+            href={"/contact"}
+            className="flex flex-row"
+            onClick={() => setIsOpen(false)}
+          >
             <p
               className={`${
                 pathname === "/contact"
